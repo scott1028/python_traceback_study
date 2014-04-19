65e33463d251d08b44bf4514740291187f35989e
scott.lan <scott.lan@taisys.com>
2014-04-19 09:36:29 +0800

    增加類似 rails has_many 的 attr_define_method 設計範例

--- ruby_class_inherit_test.rb
+++ ruby_class_inherit_test.rb
@@ -3,8 +3,33 @@
 # This will be easier to understand once you know what attr_accessor does.
 # The code attr_accessor :name is equivalent to these two methods (getter and setter)
 # def x=(value) as setter, def x as getter
+# 其實這些 attr_define_method Action 都可以模組化。
 
 require 'debugger'
+
+module M1
+	def echo
+		puts 'be include by M1 through by add_method'
+	end
+end
+
+# 模擬 rails has_many attr_define_method 做的事情
+def add_method(*module_name)
+
+	# debugger
+	module_name.each do |i|
+		include i #module_name
+	end
+
+	attr_accessor :f    # 透過自定的 attr_method 來增加成員
+end
+
+# 自定一個類似 has_many 的 attr_define_method
+def has_many2(*field)
+	field.each do |f|
+		attr_accessor f
+	end
+end
 
 class Aa
 	# 可以一行一行寫, 都會有效
@@ -19,6 +44,8 @@ end
 class Bb < Aa
 	attr_accessor :c
 	attr_accessor :p  # 不值
+	add_method M1
+	has_many2 :q, :w, :e, :r
 
 	# 效果如同 attr_accessor
 	def x=(value)
